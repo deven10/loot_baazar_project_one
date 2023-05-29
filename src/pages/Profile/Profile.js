846416f85c10f566801caa3eb3cceec13d5c6aa3
Deven Umrania <umraniadeven10@gmail.com>
2023-05-30 01:28:07 +0530

    update: styling

--- src/pages/Profile/Profile.js
+++ src/pages/Profile/Profile.js
@@ -4,19 +4,34 @@ import "react-tabs/style/react-tabs.css";
 import "./Profile.css";
 
 export const Profile = () => {
-  const user = localStorage.getItem("user");
+  const user = JSON.parse(localStorage.getItem("user"));
+  console.log("user ", user);
   return (
     <div className="profile container">
       <h2>Account</h2>
       <div className="tabs">
         <Tabs>
           <TabList>
-            <Tab>Title 1</Tab>
-            <Tab>Title 2</Tab>
+            <Tab>User Profile</Tab>
+            <Tab>Manage Address</Tab>
           </TabList>
 
-          <TabPanel>
-            <h2>Any content 1</h2>
+          <TabPanel className="tabPanel">
+            <div className="tab-div">
+              <p>
+                <span>Firstname:</span> {user.firstName}
+              </p>
+            </div>
+            <div className="tab-div">
+              <p>
+                <span>Lastname:</span> {user.lastName}
+              </p>
+            </div>
+            <div className="tab-div">
+              <p>
+                <span>Email:</span> {user.email}
+              </p>
+            </div>
           </TabPanel>
           <TabPanel>
             <h2>Any content 2</h2>
